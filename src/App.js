bd75ba7ec4753d80cad0855b2f8329de86a902d4
Karina Pospos <114627942+Kary1661@users.noreply.github.com>
2024-04-19 14:55:59 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import SearchResults from './pages/Search_results/SearchResults'
 import './App.css';
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://localhost:3001/';
+//axios.defaults.baseURL = 'http://localhost:3001/';//
 
 
 function App() {
